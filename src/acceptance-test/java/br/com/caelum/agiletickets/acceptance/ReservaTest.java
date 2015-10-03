5b853ef62641c717f8ebf22f6b801d5f4936eb20
Bruno Mortella <bmortella@gmail.com>
2015-10-03 13:08:40 -0300

    Adicao do teste de reserva ReservaTest

---
+++ src/acceptance-test/java/br/com/caelum/agiletickets/acceptance/ReservaTest.java
@@ -0,0 +1,39 @@
+package br.com.caelum.agiletickets.acceptance;
+
+import static org.hamcrest.Matchers.containsString;
+import static org.junit.Assert.assertThat;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.firefox.FirefoxDriver;
+
+public class ReservaTest {
+	private static WebDriver browser;
+	private static final String BASE_URL = "http://localhost:8080";
+
+	@Before
+	public void setUp() throws Exception {
+		browser = new FirefoxDriver();
+	}
+
+	@After
+	public void teardown() {
+		browser.close();
+	}
+
+	@Test
+	public void selecionaIngresso() throws InterruptedException {
+		browser.get(BASE_URL + "/sessao/6");
+		browser.findElement(By.name("quantidade")).sendKeys("1");
+		WebElement div = browser.findElement(By.id("content"));
+		WebElement form = div.findElement(By.tagName("form"));
+		form.submit();
+		WebElement mensagem = browser.findElement(By.id("message"));
+		assertThat(mensagem.getText(), containsString("Sessão reservada com sucesso por R$ 55,00"));
+	}
+	
+}
