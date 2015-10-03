f5c63670d527f189d6a9098e80065428c7684a2d
Bruno Mortella <bmortella@gmail.com>
2015-10-03 14:59:06 -0300

    Retirando throws inútil

--- src/acceptance-test/java/br/com/caelum/agiletickets/acceptance/ReservaTest.java
+++ src/acceptance-test/java/br/com/caelum/agiletickets/acceptance/ReservaTest.java
@@ -26,7 +26,7 @@ public class ReservaTest {
 	}
 
 	@Test
-	public void reservaIngressoEChecaAcrescimo() throws InterruptedException {
+	public void reservaIngressoEChecaAcrescimo(){
 		browser.get(BASE_URL + "/sessao/6");
 		browser.findElement(By.name("quantidade")).sendKeys("1");
 		WebElement div = browser.findElement(By.id("content"));
